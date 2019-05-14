8ffc7d12044db7a403e27eaa02e0ce7ae45961eb
José Gomes <zemiguelgomes99@gmail.com>
2019-05-14 20:00:06 +0100

    Ainda nao sei mexer no git

--- server/server.c
+++ server/server.c
@@ -20,29 +20,11 @@ int main(int argc, char* argv[]) {
 
     int num_threads = atoi(argv[1]);
 
-<<<<<<< HEAD
   char * password = argv[2];
   if (strlen(password) > MAX_PASSWORD_LEN || strlen(password) < MIN_PASSWORD_LEN) {
     printf("Wrong password size\n");
     return -2;
   }
-=======
-    if (num_threads > MAX_BANK_OFFICES) {
-        printf("Too many offices\n");
-        return -1;
-    }
-
-    char * password = argv[2];
-    if (strlen(password) > MAX_PASSWORD_LEN || strlen(password) < MIN_PASSWORD_LEN) {
-        printf("Wrong password size\n");
-        return -2;
-    }
-    else {
-        password +=1;
-        password -=1;
-    }
-
->>>>>>> user_dev
 
 //  unlink(SERVER_FIFO_PATH);
 //CRIACAO DE THREADS, PRECISAMOS DE UMA FUNCAO: func FOI O QUE PUS PROVISORIAMENTE
@@ -57,13 +39,8 @@ int main(int argc, char* argv[]) {
     }
 
 //FIFO PARA COMUNICAÇÃO, AINDA ERRADO POR CAUSA DO NOME
-<<<<<<< HEAD
   int fd;
   if(mkfifo(SERVER_FIFO_PATH, 0660) != 0) {
-=======
-
-    if(mkfifo(SERVER_FIFO_PATH, 0660) != 0) {
->>>>>>> user_dev
         fprintf(stderr, "Error creating fifo\n");
         return -3;
     }
@@ -87,7 +64,6 @@ int main(int argc, char* argv[]) {
     fd = open(response_fifo, O_RDONLY );
     write(fd,"\nBoiolas\n",19);
 
-<<<<<<< HEAD
   fd = open(SERVER_FIFO_PATH, O_RDONLY | O_NONBLOCK);
 
 
@@ -95,9 +71,6 @@ int main(int argc, char* argv[]) {
 //  struct Queue * queue = createQueue(100);
 
 
-=======
-    close(fd);
->>>>>>> user_dev
 
 //CHAMAR FUNCAO DE CRIAÇÃO DE CONTA DO ADMIN
     unlink(SERVER_FIFO_PATH);
